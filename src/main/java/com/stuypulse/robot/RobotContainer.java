cc95eeede3766d56574650d834c8853e27734ff1
an6reww <an6reww@gmail.com>
2022-12-19 17:54:13 -0500

    Add simulation methods and finish  constructor

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package com.stuypulse.robot;
 
 import com.stuypulse.robot.commands.auton.DoNothingAuton;
 import com.stuypulse.robot.constants.Ports;
+import com.stuypulse.robot.subsystems.armSim;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
 
@@ -21,6 +22,7 @@ public class RobotContainer {
     public final Gamepad operator = new AutoGamepad(Ports.Gamepad.OPERATOR);
     
     // Subsystem
+    public final armSim arm = new armSim();
 
     // Autons
     private static SendableChooser<Command> autonChooser = new SendableChooser<>();
@@ -43,7 +45,10 @@ public class RobotContainer {
     /*** BUTTONS ***/
     /***************/
 
-    private void configureButtonBindings() {}
+    private void configureButtonBindings() {
+        driver.getTopButton();
+
+    }
 
     /**************/
     /*** AUTONS ***/
---
+++ src/main/java/com/stuypulse/robot/subsystems/armSim.java
@@ -0,0 +1,91 @@
+package com.stuypulse.robot.subsystems;
+
+import com.stuypulse.stuylib.control.feedback.PIDController;
+import com.stuypulse.stuylib.network.SmartNumber;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class armSim extends SubsystemBase{
+
+  private final SingleJointedArmSim armSim;
+  private final MechanismLigament2d armArm;
+  private final SmartNumber driverState;
+
+  private final double m_armMass = Units.lbsToKilograms(11); // Kilograms
+  private final double m_armLength = Units.inchesToMeters(20.5);
+  private static final double m_gear = 9;
+
+  // private final Controller controller;
+  private final SmartNumber targetAngle;
+
+
+
+  // gearing =  9:1
+  // moment of inertia = 
+  // arm length =  20.5 inches
+  // min 0 max 90
+  // mass : 11 lbs
+    public armSim() {
+
+      setSubsystem("Arm");
+      
+      armSim =
+      new SingleJointedArmSim(
+      DCMotor.getNEO(1)
+      , m_gear
+      , SingleJointedArmSim.estimateMOI(m_armLength, m_armMass)
+      , m_armLength
+      , Units.degreesToRadians(0)
+      , Units.degreesToRadians(90)
+      , m_armMass
+      ,true);
+
+      driverState = new SmartNumber("Arm/Driver Simulation State", 0);
+
+      Mechanism2d arm = new Mechanism2d(m_armMass, m_armLength);
+      MechanismRoot2d armRoot = arm.getRoot("Intake Root", m_armMass, m_armLength);
+      armArm = new MechanismLigament2d("Arm Arm", 1, 45);
+      armRoot.append(armArm);
+      addChild("Arm Mechanism2d", arm);
+
+      targetAngle = new SmartNumber("Arm/Target Angle", 0);
+    }
+
+      @Override
+      public void simulationPeriodic() {
+        armSim.update(0.02);
+        armSim.setInputVoltage(6);
+      }
+
+      public void reset (double degrees){
+        armSim.setState(VecBuilder.fill(degrees,0));
+      }
+
+      public void stop(){
+        driverState.set(0);
+      }
+
+      public void setAngle(double angle){
+        targetAngle.set(angle);
+      }
+
+      // public double getTargetAngle(){
+      //   return targetAngle.get();
+      // }
+
+      public double getAngleDegrees(){
+        return armSim.getOutput(0);
+      }
+
+      
+
+      
+}
+
